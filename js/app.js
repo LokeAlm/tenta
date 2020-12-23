c368db4ac16dd6f6f94f53b80a04a19042c5f386
LokeAlm <69800439+LokeAlm@users.noreply.github.com>
2020-12-23 15:14:56 +0100

    cleaned up and added comments

--- js/app.js
+++ js/app.js
@@ -1,24 +1,24 @@
 import Api from './api.js'
 import Ui from './ui.js'
 
+/* runs when the page has loaded. gets tasks from json-file and adds event listener to taskInput */    
+(function getTasks(){
 
-const taskInput = document.querySelector('.add-btn');
+    const taskInput = document.querySelector('.add-btn');
     taskInput.addEventListener('click', function(e){
         e.preventDefault();
         let taskName = document.querySelector('.todo-input');
         let text = taskName.value; 
         const task = new Task(text);
-        Ui.renderToDOM(task);
-        taskName.value = '';
+        Ui.renderToDOM(task); // send new instance of Task to method in Ui
+        taskName.value = ''; // empty input field
     });
 
-    
-(function getTasks(){
     new Api();
 }())
 
 
-
+// class for constructing Task-objects
 class Task {
     constructor(taskName){
         this.name = taskName;
--- js/ui.js
+++ js/ui.js
@@ -5,6 +5,7 @@ class Ui {
     static renderToDOM(data){
         const list = document.querySelector('.list');
 
+        // if data is an array, it comes from json:
         if(data.length > 1){
             for(let i=0; i<data.length; i++){
 
@@ -16,7 +17,9 @@ class Ui {
                                 <button class="btn delete-btn"><i class="fas fa-trash-alt"></i></button>
                             </li>`     
             }
-        } else {
+
+        // if the data comes from user input:
+        } else if(data.name != '') { // also check so user input is not empty
             list.innerHTML += `
                 <li class="list-item">
                     <div class="task">
@@ -26,13 +29,14 @@ class Ui {
                     <button class="btn delete-btn"><i class="fas fa-trash-alt"></i></button>
                 </li>`
         }
-        
-        document.querySelectorAll('.delete-btn').forEach(element => element.addEventListener('click', Ui.deleteItem));
 
+        // adds event listeners to all delete-buttons
+        document.querySelectorAll('.delete-btn').forEach(element => element.addEventListener('click', Ui.deleteItem));
+        // adds event listeners to all checkboxes
         document.querySelectorAll('.check').forEach(element => element.addEventListener('click', Ui.toggleDone));
     }
 
-    static deleteItem(element){
+    static deleteItem(element){ // 'element' is i-element
         const list = document.querySelector('.list');
         const li = element.target.parentNode.parentNode;
         list.removeChild(li);
